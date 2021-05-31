a00b057e97234da8da8883a5e75c10b8cc2a2e6d
Jhonatan Sena <jcostasenna@hotmail.com>
2021-05-31 08:21:59 -0300

    Criado o método de atualização de pessoa

--- src/main/java/io/github/jhonatansena/personapi/service/PersonService.java
+++ src/main/java/io/github/jhonatansena/personapi/service/PersonService.java
@@ -34,7 +34,7 @@ public class PersonService {
         Person personToSave = personMapper.toModel(personDTO);
 
         Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO.builder().message("Created person with ID "+ savedPerson.getId()).build();
+        return createMessageResponse(savedPerson.getId(), " Created person with ID ");
     }
 
 
@@ -61,10 +61,28 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException{
+
+        verifyIfAlredyExists(id);
+
+        Person personToUpdate = personMapper.toModel(personDTO);
+
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createMessageResponse(updatedPerson.getId(), " Created person with ID ");
+    }
+
     private Person verifyIfAlredyExists(Long id)throws PersonNotFoundException{
 
         return personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message){
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
+
     }
 
 
